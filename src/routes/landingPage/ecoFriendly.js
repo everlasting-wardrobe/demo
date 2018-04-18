203ade7f70f9acd10ef3528e15d5ebfea5f6c114
Cody Fang <fangce1063@gmail.com>
2018-04-18 19:17:41 -0400

    starting ecofriendly

--- src/routes/landingPage/ecoFriendly.js
+++ src/routes/landingPage/ecoFriendly.js
@@ -1,19 +1,102 @@
 import React from 'react'
 import {StyleSheet, css} from 'aphrodite'
 import {Grid, Row, Col} from 'react-bootstrap'
+import EcoFriendlyImg from '../../imgs/Eco_Friendly_Image.jpg'
+// const styles = StyleSheet.create({
+//   benefitWrapper:{
+//     textAlign:'center'
+//   },
+//   grid:{
+//     width: '80%'
+//   }
+// })
+
 
 const styles = StyleSheet.create({
-  benefitWrapper:{
-    textAlign:'center'
+  EcoContainerWrapper : {
+    textAlign:'center',
+    backgroundImage: `url(${EcoFriendlyImg})`,
+    backgroundSize: `100% 100%`,
+    height: '800px',
   },
-  grid:{
-    width: '80%'
+  head: {
+    marginTop: '0px',
+    paddingTop: '20px',
+    paddingBottom: '15px',
+    color:  'white',
+    fontSize: '90px',
+    fontFamily: 'Sacramento',
+  },
+  grid: {
+    width: '80%',
+    paddingLeft:'0px',
+    paddingRight:'0px',
+  },
+  row: {
+
+  },
+  col: {
+    marginLeft: '5.15%',
+    marginRight: '5.15%',
+    paddingLeft: '0px',
+    paddingRight:'0px',
+    color: 'white',
+  },
+  pricingButtonWrapper:{
+    height: '550px',
+    backgroundColor: 'rgba(232, 90, 79, 0.8)',
+    borderRadius:'15px',
+    opacity:'70%',
+    margin:'auto',
+    width: '100%',
+  },
+
+  content: {
+      margin : 'auto',
+      paddingLeft: '5%',
+      paddingRight: '5%',
+  },
+
+  pricingHead:{
+    paddingTop: '15%',
+    color: 'white',
+    paddingBottom: '15px',
+    marginBottom:'0px',
+    fontFamily: 'Open Sans',
+    fontSize: '65px',
+  },
+
+  quantity: {
+    fontSize: '48px',
+  },
+
+  price:{
+    fontSize: '80px',
+    paddingTop: '25px',
+    fontFamily: 'Lato',
+    fontStyle: 'italic, thin',
+  },
+
+  period:{
+    fontSize: '25px',
+    fontFamily: 'Lato',
+    fontStyle: 'italic',
+  },
+
+  detail: {
+    fontSize: '28px',
+    fontFamily: 'Lato',
+    fontStyle: 'italic',
+    marginLeft: '5%',
+    marginRight: '5%',
   }
 })
 
+
+
 const EcoFriendly = () => {
   return (
-    <div className={css(styles.benefitWrapper)}>
+    <div className={css(styles.EcoContainerWrapper)}>
       <h2>Eco-friendly and Recycling</h2>
       <Grid className={css(styles.grid)}>
         <Row>
