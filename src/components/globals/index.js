66e5941321166180669c3dc41a3cf68489c312f1
Xiaoxu <xl1842@nyu.edu>
2018-08-08 13:56:53 -0400

    Test styled-component

---
+++ src/components/globals/index.js
@@ -0,0 +1,23 @@
+import styled from 'styled-componnets';
+
+
+
+export const FlexCol = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+`;
+
+export const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+
+export const Img = styled.img`
+  width: 100%;
+  height: 100%;
+`
