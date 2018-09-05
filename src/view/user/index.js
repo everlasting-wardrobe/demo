e55440037314b39d0c3e42d7a4bea82611e4e979
Xiaoxu <xl1842@nyu.edu>
2018-09-05 16:21:30 -0400

    Add routes

--- src/view/user/index.js
+++ src/view/user/index.js
@@ -3,12 +3,22 @@ import {Signup} from './signup';
 import {HowItWork} from './howItWork';
 import Navbar from '../navbar/navbar';
 import Footer from '../footer/footer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 const UserManagement = () => {
   return (
     <div>
       <Navbar />
-      <HowItWork />
+      <Router>
+        <Switch>
+          <Route path={'/test/test/how-it-works'} component={HowItWork} />
+          <Route path={'/test/test/signup'} component={Signup} />
+        </Switch>
+      </Router>
       <Footer />
     </div>
   )
