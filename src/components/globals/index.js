21ebff77b11702af63bef2a084a4e682946e1f80
zhixin wen <zw1347@nyu.edu>
2018-09-05 12:06:06 -0400

    modify navbar in sign in page

--- src/components/globals/index.js
+++ src/components/globals/index.js
@@ -54,23 +54,26 @@ export const NavTab = styled(Link)`
   }
 `;
 
-export const NavList = styled.ul`
-  width: 100%;
+export const NavListWrapper = styled.ul`
   list-style: none;
-  float: left;
   > li {
-    display: block;
+    ${props =>
+      props.floated && css
+      `
+        width: auto;
+        float: left;
+      `}
   }
 `;
 
-export const SocialList = styled.div`
-  display: flex;
+export const SocialListWrapper = styled.div`
   padding-left: 10px;
-  margin-bottom: 10px;
+  margin: 5px 0;
+  display: flex;
+  flex-direction: row;
    img {
     max-height: 40px;
     height: 28px;
-    width: auto;
     opacity: .6;
     margin-right: 7px;
     transition: opacity .3s ease;
@@ -92,26 +95,69 @@ export const Dropdown = styled.div`
     `
       max-height: 0;
     `}
+  
+  @media (min-width: 1200px) {
+    display: none;
+  }
 `;
 
-export const EWNavLogoImg = styled(Link)`
-  display: inline-block;
+export const EWNavLogo = styled(Link)`
+  h3 {
+    display: inline-block;
+    font-family: 'Lato';
+    font-weight: 300;
+    font-size: 20px;
+    margin: 0;
+    color: #e75a50;
+  }
 `;
 
-export const NavBrand = styled.div`
+export const TopBarWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
   padding: 13px 5px;
   font-size: 20px;
   height: 100%;
-  width: auto;
+  border-bottom: 1px solid #e7e7e7;
   a {
     height: 100%;
   }
   a img {
     height: 100%;
   }
+  button {
+    float: right;
+  }
 `;
 
 export const NavBar = styled.div`
   height: 66px;
   background-color: #FCFCF5;
+`;
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${props =>
+    props.flexInRow && css
+    ` 
+    flex-direction: row;
+    `}
+`;
+
+export const BrandName = styled.h3`
+  font-family: 'Lato';
+  font-size: 20px;
+  margin: 0;
+  color: #e75a50;
+  display: inline-block;
+  span {
+    font-weight: 400;
+  }
+`;
+
+export const RightLinks = styled.div`
+
+  @media (max-width: 1200px) {
+    display: none;
+  }
 `;
--- src/routes/landingPage/navbar.js
+++ src/routes/landingPage/navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { NavTab, NavList, SocialList, Dropdown, EWNavLogoImg, NavBrand, NavBar } from '../../components/globals/index';
+import { NavTab, NavListWrapper, SocialListWrapper, Dropdown, EWNavLogo, TopBarWrapper, NavBar, ListContainer, RightLinks } from '../../components/globals/index';
 import Link from '../../components/link';
 
 import snapchat from '../../imgs/snapchatLogo.svg';
@@ -10,7 +10,55 @@ import twitter from '../../imgs/twitterLogo.svg';
 import pinsterest from '../../imgs/pinterestLogo.svg';
 import EWLogo from '../../imgs/EWLogo.svg';
 
+const NavList = (props) => (
+    <ListContainer flexInRow={props.inRow}>
+        <NavListWrapper floated={props.inRow}>
+            <li>
+                <NavTab to={""}>How It Works</NavTab>
+            </li>
+            <li>
+                <NavTab to={""}>Sign up</NavTab>
+            </li>
+            <li>
+                <NavTab to={""}>Log in</NavTab>
+            </li>
+        </NavListWrapper>
+        <SocialList />
+    </ ListContainer>
+);
 
+const SocialList = () => (
+    <SocialListWrapper>
+        <Link to={""}>
+            <img src={snapchat} />
+        </Link>
+        <Link to={""}>
+            <img src={facebook} />
+        </Link>
+        <Link to={""}>
+            <img src={instgram} />
+        </Link>
+        <Link to={""}>
+            <img src={twitter} />
+        </Link>
+        <Link to={""}>
+            <img src={pinsterest} />
+        </Link>
+    </SocialListWrapper>
+);
+
+const BrandCombo = () => (
+    <div>
+        <EWNavLogo to={""}>
+            <img src={EWLogo} />
+        </EWNavLogo>
+        <EWNavLogo to={""}>
+            <h3>
+                {"EverlastingWardrobe"}
+            </h3>
+        </EWNavLogo>
+    </div>
+);
 
 class Navbar extends Component {
     state = {
@@ -24,41 +72,15 @@ class Navbar extends Component {
     render() {
         return (
             <NavBar>
-                <NavBrand>
-                    <EWNavLogoImg to={""}>
-                        <img src={EWLogo} />
-                    </EWNavLogoImg>
-                    <button onClick={this.dropdownToggler}>toggle</button>
-                </NavBrand>
+                <TopBarWrapper>
+                    <BrandCombo />
+                    <RightLinks>
+                    <NavList inRow/>
+                    </RightLinks>
+                    {/* <button onClick={this.dropdownToggler}>toggle</button> */}
+                </TopBarWrapper>
                 <Dropdown collapse={this.state.closed}>
-                    <NavList>
-                        <li>
-                            <NavTab to={""}>How It Works</NavTab>
-                        </li>
-                        <li>
-                            <NavTab to={""}>Sign up</NavTab>
-                        </li>
-                        <li>
-                            <NavTab to={""}>Log in</NavTab>
-                        </li>
-                    </NavList>
-                    <SocialList>
-                        <Link to={""}>
-                            <img src={snapchat} />
-                        </Link>
-                        <Link to={""}>
-                            <img src={facebook} />
-                        </Link>
-                        <Link to={""}>
-                            <img src={instgram} />
-                        </Link>
-                        <Link to={""}>
-                            <img src={twitter} />
-                        </Link>
-                        <Link to={""}>
-                            <img src={pinsterest} />
-                        </Link>
-                    </SocialList>
+                    <NavList />
                 </Dropdown>
                 sign up box
             </NavBar>
